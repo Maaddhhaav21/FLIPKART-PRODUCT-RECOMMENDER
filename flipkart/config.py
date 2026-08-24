e397f67c2c6c9696a14b51b5f0bfd0dbdf005217
Madhav Manoj <maadhavmanoj21@gmail.com>
2026-08-24 22:24:45 +0530

    Config done

--- flipkart/config.py
+++ flipkart/config.py
@@ -8,4 +8,4 @@ class Config:
     ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     EMBEDDINGS_MODEL = "BAAI/bge-base-en-v1.5"
-    RAG_MODEL = "llama-3.1-8b-instant"
+    RAG_MODEL = "openai/gpt-oss-20b"
